0296b820dd2ee6b5cf18e0e9e56a079d62767d6a
ebra8 <notebra8@gmail.com>
2025-12-02 10:11:20 +0200

    Channels have realtime messages that belong to user

--- db/schema.rb
+++ db/schema.rb
@@ -27,16 +27,6 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_02_072513) do
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
-  create_table "solid_cable_messages", force: :cascade do |t|
-    t.binary "channel", limit: 1024, null: false
-    t.integer "channel_hash", limit: 8, null: false
-    t.datetime "created_at", null: false
-    t.binary "payload", limit: 536870912, null: false
-    t.index ["channel"], name: "index_solid_cable_messages_on_channel"
-    t.index ["channel_hash"], name: "index_solid_cable_messages_on_channel_hash"
-    t.index ["created_at"], name: "index_solid_cable_messages_on_created_at"
-  end
-
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email", default: "", null: false
