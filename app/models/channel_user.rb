98bab81c428153b9e403ffddcbe0d6c9f04a9a42
ebra8 <notebra8@gmail.com>
2025-12-04 10:22:16 +0200

    users join/leave in realtime

--- app/models/channel_user.rb
+++ app/models/channel_user.rb
@@ -1,14 +1,22 @@
 class ChannelUser < ApplicationRecord
   belongs_to :channel
   belongs_to :user
+  after_create :broadcast_members
+  after_destroy :remove_broadcast_members
 
-  # after_create :broadcast_members
-  # def broadcast_members
-  #   broadcast_append_to(
-  #     [ channel, "users" ],
-  #     target: "channel_members",
-  #     partial: "users/user",
-  #     locals: { user: user }
-  #   )
-  # end
+  def broadcast_members
+    broadcast_append_to(
+      [ channel, "users" ],
+      target: "channel-members",
+      partial: "users/user",
+      locals: { user: user }
+    )
+  end
+
+  def remove_broadcast_members
+    broadcast_remove_to(
+      [ channel, "users" ],
+      target: "user_#{user.id}"
+    )
+  end
 end
